f90e1ef2623ba39080082b398ea354d5a0cda989
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-01-13 20:20:50 +0700

    update model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,29 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eos vero facere quas! Exercitationem laborum omnis id eius. Inventore temporibus ipsam explicabo autem, laborum saepe. Suscipit, mollitia. Sint, perspiciatis eligendi.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eos vero facere quas! Exercitationem laborum omnis id eius. Inventore temporibus ipsam explicabo autem, laborum saepe. Suscipit, mollitia. Sint, perspiciatis eligendi.'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
