f1b4a3b706e48ed6aa9afcd02a6ceed82d0d54c2
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-01-14 18:04:59 +0700

    add listing form, form validation, store listing to database

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,14 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
