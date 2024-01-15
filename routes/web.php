6cf1dd81a8ba30265c14de4a74d82bca4363f422
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-01-15 19:39:32 +0700

    add delete listing feature

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,8 @@ Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 // Update Listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
-
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
